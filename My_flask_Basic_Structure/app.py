ef3e24b1eaa0e53558d2e97b145837faa011d0d0
AdapaManiKanta <manikanta.adapa15@gmail.com>
2025-07-23 22:34:32 +0530

    Portfolio is updated in Login_Authentication

--- My_flask_Basic_Structure/app.py
+++ My_flask_Basic_Structure/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("testing.html",content=["Hello", "World", "Flask", "is", "awesome"])
 
 if __name__ in "__main__":
     app.run(debug=True)
